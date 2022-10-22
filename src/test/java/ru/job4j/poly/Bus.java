ed95cb130ffee2b4962ceb4451c3049cfc599840
Yoshimitzu87 <spitfires111@gmail.com>
2022-10-22 11:13:07 +0300

    3. Интерфейс Input

--- src/test/java/ru/job4j/poly/Bus.java
+++ src/test/java/ru/job4j/poly/Bus.java
@@ -4,10 +4,12 @@ public class Bus implements Transport {
 
     @Override
     public void ride() {
+        System.out.println("RIDE");
     }
 
     @Override
     public void passengers(int passCount) {
+        System.out.println(passCount);
     }
 
     @Override
